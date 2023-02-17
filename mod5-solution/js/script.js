fbc33342726d579e820f7088d4afe571fb223370
BrendelDrT <tb@brendel.consulting>
2023-02-17 14:55:44 +0100

    extra console log

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -120,6 +120,7 @@ function buildAndShowHomeHTML (categories) {
           homeHtml,
           "randomCategoryShortName",
           chosenCategoryShortName.short_name)
+      console.log("homeHtml..." + homeHtmlToInsertIntoMainPage)
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
